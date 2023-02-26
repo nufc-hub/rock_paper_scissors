13d02f31f7341f563cddbb1177e5ace72110899c
nufc-hub <kmchenouf@gmail.com>
2023-02-26 23:34:19 +0900

    Play one round of rock paper scissors and return a result.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,8 +1,9 @@
+let rock = 'rock';
+let paper = 'paper';
+let scissors = 'scissors';
 
 function getComputerChoice() {
-    const rock = 'rock';
-    const paper = 'paper';
-    const scissors = 'scissors';
+   
     //create three variables//
    //rock, paper, scissors
     
@@ -26,7 +27,69 @@ function getComputerChoice() {
     return computerOutput;
 }
 
-console.log(getComputerChoice())
 
 
 
+
+function playRound (playerSelection, computerSelection) {
+    let roundResult;
+    let playerScore = 0;
+    let computerScore = 0;
+   if (playerSelection === rock && computerSelection === scissors) {
+    roundResult = "Rock beats scissors, you win!";
+    playerScore = 1;
+   } else if (playerSelection === paper && computerSelection === rock) {
+    roundResult = "Paper beats rock, you win!";
+    playerScore = 1;
+   } else if (playerSelection === scissors && computerSelection === paper) {
+    roundResult = "Scissors beats paper, you win!";
+    playerScore = 1
+   } else if (playerSelection === rock && computerSelection === rock) {
+    return "Rock and rock, you draw.";
+   } else if (playerSelection === paper && computerSelection === paper) {
+    return "Paper and Paper, you draw.";
+   } else if (playerSelection === scissors && computerSelection === scissors) {
+    return "Scissors and scissors, you draw.";
+   } else if (playerSelection === rock && computerSelection === paper) {
+    roundResult = "You lose, paper beats rock";
+    computerScore = 1;
+   } else if (playerSelection === paper && computerSelection === scissors) {
+    roundResult = "You lose, scissors beats paper.";
+    computerScore = 1;
+   } else if (playerSelection === scissors && computerSelection === rock) {
+    roundResult = "You lose, rock beats scissors.";
+    computerScore = 1;
+   } else {
+    return "Error, something went wrong."
+   }
+
+   if (playerScore === 1){
+    return {
+        playerScore: playerScore,
+        computerScore: computerScore,
+        message: roundResult + " Your score is now " + playerScore + 
+        " and the computer score is now " + computerScore + "."
+    };
+   } else if (computerScore === 1){
+    return {
+        playerScore: playerScore,
+        computerScore: computerScore,
+        message: roundResult + " Your score is now " + playerScore + 
+        " and the computer score is now " + computerScore + "."
+    };
+   } else {
+    return {
+        playerScore: playerScore,
+        computerScore: computerScore,
+        message: roundResult + " Your score is still " + playerScore + 
+        " and the computer score is still " + computerScore + "."
+    };
+   }
+   
+}
+
+let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+let computerSelection = getComputerChoice();
+
+
+console.log(playRound(playerSelection, computerSelection))
