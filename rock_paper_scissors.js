37e3335e181c971b4b433096d180a74b5b50331e
nufc-hub <kmchenouf@gmail.com>
2023-03-15 22:16:15 +0900

    Add javascript to html
    Removed logic that plays five rounds.
    Created three buttons for each selection in html.
    Added an event listener to the buttons that calls playRound function.
    Added divs for displaying results.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -30,44 +30,18 @@ function playRound (playerSelection, computerSelection) {//this plays one round 
    return {message: roundResult, playerScore, computerScore};//This will always return an object even if an invalid input is provided
 
    }
+   
+   let roundResultMsg;
+   const resultBox = document.querySelector('.result');
 
+   let buttons = document.querySelectorAll('.btn');
+   buttons.forEach(button => {
+    button.addEventListener('click', function(event) {
+        const playerChoice = event.target.textContent.toLowerCase();
+        const computerChoice = getComputerChoice();
+        const roundResult = playRound(playerChoice, computerChoice);
+        roundResultMsg = roundResult.message;
 
-function game() {// this plays a five round game of rock, paper, scissors
-    let playerScore = 0;// playerScore starts at 0
-    let computerScore = 0;//computerScore starts at 0
-
-    for (let i = 1; i < 6; i++) { //this gives us five rounds. It is set to 1 so that roundNumber(variable) always starts at round 1.
-        let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();/*this is displayed five times, if the user inputs 
-                                                                                 a word from the 'choices' array*/
-        let computerSelection = getComputerChoice();
-        let roundNumber = `Round ${i}`;//this shows what round we just played
-        console.log(roundNumber)
-        
-        let round;/*this incorporates a function that plays one round of the game, due to the while loop, if an invalid answer is input, 
-                    you cant proceed to the next round until a valid input is entered*/
-        while (true) {
-            round = playRound(playerSelection, computerSelection);
-            if (round.message != "Please enter rock, paper or scissors.") {
-                break;
-            }
-            console.log("Please try again.");
-            playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
-        } 
-        console.log(round.message);
-
-        playerScore += round.playerScore; //this returns the score to playerScore after each round
-        computerScore += round.computerScore; ////this returns the score to playerScore after each round
-        console.log(`Player score: ${playerScore}, Computer score: ${computerScore}`);//this shows the score at the end of each round
-    }
-    
-//these are the conditions that take place at the end of the fifth round
-    if (playerScore > computerScore) {
-        return "You win the game.";
-    } else if (playerScore === computerScore) {
-        return "You draw the game.";
-    } else {
-        return "You lose the game.";
-    }
-}
-
-console.log(game())
+        resultBox.textContent = roundResultMsg;
+    })
+   });
