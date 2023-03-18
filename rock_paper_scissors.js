4584528c36c63674cee06b33b403b9b6c90d29f4
nufc-hub <kmchenouf@gmail.com>
2023-03-18 10:54:06 +0900

    Create an interactive webpage to play a game of RPS
    This commit adds javaScript code to create a game of rock, paper,
    scissors in a webpage.
    
    It allows the player to make moves and keep track of the score.
    
    The HTML was updated and CSS classes added to create a responsive
    design.
    
    A play game function was added that includes DOM manipulation. It also
    includes:
    
    A game end when a player reaches a score of 5. The RPS buttons then
    disable and a the round result message changes to an end game message.
    
    A reset button that can reset the player scores to 0. This can be done
    at any point during the game.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"]; //array with three choices
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() { //picks one of the choices in the array at random
     const randomIndex = Math.floor(Math.random() * choices.length );
@@ -8,40 +10,66 @@ function getComputerChoice() { //picks one of the choices in the array at random
 
 function playRound (playerSelection, computerSelection) {//this plays one round of the game
     let roundResult;
-    let playerScore = 0;
-    let computerScore = 0;
-    
-   if (!choices.includes(playerSelection)) {
-    roundResult = "Please enter rock, paper or scissors."
-   } else if (playerSelection === computerSelection) {
-    roundResult = `It's a draw, both players chose ${playerSelection}.`
-   } else if (
-   (playerSelection === 'rock' && computerSelection === 'scissors') ||
-   (playerSelection === 'paper' && computerSelection === 'rock') ||
-   (playerSelection === 'scissors' && computerSelection === 'paper')
-   ) {
-    roundResult = `You win. ${playerSelection} beats ${computerSelection}`;
-    playerScore = 1;
-   } else {
-    roundResult = `You lose. ${computerSelection} beats ${ playerSelection}`;
-    computerScore = 1;
-   }
+  
+    if (playerSelection === computerSelection) {
+        roundResult = `It's a draw, both players chose ${playerSelection}.`
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        roundResult = `You win. ${playerSelection} beats ${computerSelection}`;
+        playerScore += 1;
+    } else {
+        roundResult = `You lose. ${computerSelection} beats ${ playerSelection}`;
+        computerScore += 1;
+    }
    
-   return {message: roundResult, playerScore, computerScore};//This will always return an object even if an invalid input is provided
+    return {message: roundResult, playerScore, computerScore};//This will always return an object even if an invalid input is provided
+}
 
-   }
-   
-   let roundResultMsg;
-   const resultBox = document.querySelector('.result');
+function playGame() {// this is to play a full game of RPS
+    const resultBox = document.querySelector('.result');
+    if (playerScore === 0 && computerScore === 0) {
+        resultBox.textContent = 'Let\'s play!';//this is to display a 'let's play' message when the game starts
+    }
+    const playerScoreBox = document.getElementById('player-score');//these are the html player score
+    const computerScoreBox = document.getElementById('computer-score');//and computer score boxes
+    const buttons = document.querySelectorAll('.btn')// the html buttons
+    let playerChoice;
 
-   let buttons = document.querySelectorAll('.btn');
-   buttons.forEach(button => {
-    button.addEventListener('click', function(event) {
-        const playerChoice = event.target.textContent.toLowerCase();
-        const computerChoice = getComputerChoice();
-        const roundResult = playRound(playerChoice, computerChoice);
-        roundResultMsg = roundResult.message;
 
-        resultBox.textContent = roundResultMsg;
-    })
-   });
+
+    buttons.forEach(button => {//Adds DOM manipulation
+        button.addEventListener('click', function(event) {
+            playerChoice = event.target.textContent.toLowerCase();/* this gets the text content of the clicked button (rock, paper or scissors),
+                                                                     converts it to lowercase, and assigns it to the playerChoice variable.*/
+            const computerChoice = getComputerChoice();
+            const roundResult = playRound(playerChoice, computerChoice);
+            const roundResultMsg = roundResult.message;
+            resultBox.textContent = roundResultMsg;//shows the round result
+            playerScoreBox.textContent = playerScore;// shows player score
+            computerScoreBox.textContent = computerScore;// shows computer score
+
+            if (playerScore === 5) {//this triggers game over
+                resultBox.textContent = 'Game over. You have won the game!';
+                buttons.forEach(button => button.disabled = true);//this disables the RPS buttons 
+            } else if (computerScore === 5) {
+                resultBox.textContent = 'Game over. You lose. please try again';
+                buttons.forEach(button => button.disabled = true);
+            }
+        })
+    });
+
+    const restartGame = document.querySelector('.restartGame');
+    restartGame.addEventListener('click', function(){//this restarts the game
+        playerScore = 0;
+        computerScore = 0;
+        playerScoreBox.textContent = playerScore;
+        computerScoreBox.textContent = computerScore;
+        resultBox.textContent = 'Let\'s play!'
+        buttons.forEach(button => button.disabled = false);//this enables the RPS buttons
+    });
+}
+
+playGame();
